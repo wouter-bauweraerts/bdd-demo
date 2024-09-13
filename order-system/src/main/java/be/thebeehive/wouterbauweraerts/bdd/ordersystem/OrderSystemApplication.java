2585068b3483011d054f37d311578f20dae9c5aa
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2024-09-13 14:44:48 +0200

    Setup test class + move main class

--- order-system/src/main/java/be/thebeehive/wouterbauweraerts/bdd/ordersystem/OrderSystemApplication.java
+++ order-system/src/main/java/be/thebeehive/wouterbauweraerts/bdd/ordersystem/OrderSystemApplication.java
@@ -1,4 +1,4 @@
-package productcatalog;
+package be.thebeehive.wouterbauweraerts.bdd.ordersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
